53eade44a7cf10a18e2e06601159f807608f7786
Benjamin Blumer <BenjaminAaronBlumer@gmail.com>
2013-10-31 18:10:37 +0000

    Deleted the floor, the table, the timed with camera. These have all been moved to my public folder so that I can upload them to github

--- IIIM_WAM/doit.sh
+++
@@ -1,6 +0,0 @@
-rm -r build
-mkdir build
-cd build
-cmake -DCMAKE_INSTALL_PREFIX=~/local ..
-make
-make install
--- WAM Model Timed With Camera/doit.sh
+++
@@ -1,8 +0,0 @@
-#!
-rm -r build
-mkdir build
-cd build 
-cmake ../
-make
-cd ..
-#gazebo wam_world.sdf
